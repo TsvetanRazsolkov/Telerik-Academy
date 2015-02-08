70870bd9f00a339b30e691ec65720bc29f6e3fcb
TsvetanRazsolkov <tsvetan.razsolkov@gmail.com>
2015-02-08 14:05:29 +0200

    Uploading the Exam2015CSharp1.sln from the C# Basics Part I exam on 03.02.2015.

---
+++ Exams/01-C# Basics - Part I/Exam2015CSharp1/Problem1/ThreeNumbers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Numerics;
+using System.Threading;
+using System.Globalization;
+
+class ThreeNumbers
+{
+    static void Main()
+    {        
+
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        double mean = (a + b + c) / 3.0;
+
+        int biggest = a;
+        if (b >= a)
+        {
+            biggest = b;
+            if (c >= b)
+            {
+                biggest = c;
+            }
+        }
+        else if (b < a)
+        {
+            if (c >= a)
+            {
+                biggest = c;
+            }
+        }
+        Console.WriteLine(biggest);
+
+        int smallest = a;
+        if (b <= a)
+        {
+            smallest = b;
+            if (c <= b)
+            {
+                smallest = c;
+            }
+        }
+        else if (b > a)
+        {
+            if (c <= a)
+            {
+                smallest = c;
+            }
+        }
+        Console.WriteLine(smallest);
+        Console.WriteLine("{0:F2}", mean);
+    }
+}
---
+++ Exams/01-C# Basics - Part I/Exam2015CSharp1/Problem3/ConsoleApplication1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Numerics;
+using System.Threading;
+using System.Globalization;
+
+class ConsoleApplication1
+{
+    static void Main()
+    {
+        int counter = 0;
+        string num = string.Empty;
+
+        BigInteger firstResult = 1;
+        BigInteger secondResult = 1;
+        while (num != "END")
+        {
+            num = Console.ReadLine();
+            if (counter < 10 && counter % 2 == 1)
+            {
+                BigInteger result = 1;
+                if (num == "0")
+                {
+                    result = 1;
+                    firstResult *= result;
+                }
+                else if (num != "END")
+                {
+                    num = num.Replace('-', '1');
+                    num = num.Replace('0', '1');
+                    for (int i = 0; i < num.Length; i++)
+                    {
+                        result *= (num[i] - '0');
+                    }
+                    firstResult *= result;
+                }
+
+            }
+            else if (counter >= 10 && counter % 2 == 1)
+            {
+                BigInteger result = 1;
+                if (num == "0")
+                {
+                    result = 1;
+                    secondResult *= result;
+                }
+                else if (num != "END")
+                {
+                    num = num.Replace('-', '1');
+                    num = num.Replace('0', '1');
+                    for (int i = 0; i < num.Length; i++)
+                    {
+                        result *= (num[i] - '0');
+                    }
+                    secondResult *= result;
+                }
+            }
+            counter++;
+        }
+        Console.WriteLine(firstResult);
+        if (counter >= 10)
+        {
+            Console.WriteLine(secondResult);
+        }
+    }
+
+}
