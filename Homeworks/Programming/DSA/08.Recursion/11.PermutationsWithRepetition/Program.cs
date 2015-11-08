236dd5d9b392fc8054e2a5c5323909a3f7e106c7
TsvetanRazsolkov <tsvetan.razsolkov@gmail.com>
2015-11-08 11:38:21 +0200

    DSA/08.Recursion homework - tasks 11 and 12 added.

---
+++ Homeworks/Programming/DSA/08.Recursion/11.PermutationsWithRepetition/Program.cs
@@ -0,0 +1,72 @@
+﻿
+/*Write a program to generate all permutations with repetitionsof given multi-set.
+Example: the multi-set {1, 3, 5, 5} has the following 12 unique permutations:
+  { 1, 3, 5, 5 }  { 1, 5, 3, 5 }
+  { 1, 5, 5, 3 }  { 3, 1, 5, 5 }
+  { 3, 5, 1, 5 }  { 3, 5, 5, 1 }
+  { 5, 1, 3, 5 }  { 5, 1, 5, 3 }
+  { 5, 3, 1, 5 }  { 5, 3, 5, 1 }
+  { 5, 5, 1, 3 }  { 5, 5, 3, 1 }
+Ensure your program efficiently avoids duplicated permutations.
+Test it with { 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 }.
+Hint: Permutations with repetition*/
+namespace _11.PermutationsWithRepetition
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static HashSet<string> answer = new HashSet<string>();
+
+        public static int[] multiSet = new int[] { 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+
+        public static void Main()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            Permute(0);
+            Console.WriteLine("Time: {0}", sw.Elapsed);
+
+            Console.WriteLine(string.Join(Environment.NewLine, answer));
+        }
+
+        public static void Permute(int startingPosition = 0)
+        {
+            answer.Add(string.Join(", ", multiSet));
+            if (startingPosition < multiSet.Length)
+            {
+                for (int i = multiSet.Length - 2; i >= startingPosition; i--)
+                {
+                    for (int j = i + 1; j < multiSet.Length; j++)
+                    {
+                        if (multiSet[i] != multiSet[j])
+                        {
+                            Swap(i, j, multiSet);
+                            Permute(i + 1);
+                        }
+                    }
+
+                    int temp = multiSet[i];
+                    for (int k = i; k < multiSet.Length - 1; k++)
+                    {
+                        multiSet[k] = multiSet[k + 1];
+                    }
+
+                    multiSet[multiSet.Length - 1] = temp;
+                }
+            }
+        }
+
+        private static void Swap(int start, int end, int[] array)
+        {
+            var temp = array[end];
+            array[end] = array[start];
+            array[start] = temp;
+        }
+    }
+}
---
+++ Homeworks/Programming/DSA/08.Recursion/12.QueensPuzzle/Program.cs
@@ -0,0 +1,63 @@
+﻿
+/*Write a recursive program to solve the "8 Queens Puzzle" with backtracking.*/
+namespace _12.QueensPuzzle
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        private const int Size = 8;
+        static int[,] board = new int[Size, Size];
+        static int countOfSolutions = 0;
+
+        public static void Main(string[] args)
+        {
+            Queens(board, 0);
+            Console.WriteLine("Count of possible solutions = {0}", countOfSolutions);
+        }
+
+        static void Queens(int[,] board, int columnIndex)
+        {
+            if (columnIndex == Size)
+            {
+                countOfSolutions++;
+                return;
+            }
+
+            for (int rowIndex = 0; rowIndex < board.GetLength(0); rowIndex++)
+            {
+                if (board[rowIndex, columnIndex] == 0) // Check if current cell is occupied
+                {
+                    board[rowIndex, columnIndex] += 1;
+                    MarkOccupied(board, 1, rowIndex, columnIndex);
+                    Queens(board, columnIndex + 1);
+                    board[rowIndex, columnIndex] -= 1; // remove the queen from where it has been placed for next placement;
+                    MarkOccupied(board, -1, rowIndex, columnIndex);
+                }
+            }
+        }
+
+        static void MarkOccupied(int[,] board, int value, int rowIndex, int columnIndex)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                board[i, columnIndex] += value;
+                board[rowIndex, i] += value;
+
+                if (columnIndex + i < board.GetLength(0) && rowIndex + i < board.GetLength(0))
+                {
+                    board[rowIndex + i, columnIndex + i] += value;
+                }
+
+                if (columnIndex + i < board.GetLength(0) && rowIndex - i >= 0)
+                {
+                    board[rowIndex - i, columnIndex + i] += value;
+                }
+            }
+        }
+    }
+}
